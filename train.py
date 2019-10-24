5e998445a4c1fa9c84d3dcda58f89904fcfabc19
vbbphc <balloli.vb@gmail.com>
2019-10-24 15:16:18 +0530

    Fix: baseline reward

--- train.py
+++ train.py
@@ -286,7 +286,7 @@ def train_controller(epoch,
         reward += args.controller_entropy_weight * controller.sample_entropy
 
         if baseline is None:
-            baseline = val_acc * latency
+            baseline = val_acc * ((latency / 100.0) ** (-0.07))
         else:
             baseline -= (1 - args.controller_bl_dec) * (baseline - reward)
             # detach to make sure that gradients are not backpropped through the baseline
