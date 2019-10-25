c48b5c9cde2cac8525b97a4a1d1dafda4270c890
vbbphc <balloli.vb@gmail.com>
2019-10-25 10:44:34 +0530

    Adds: Average CUDA latency

--- utils/utils.py
+++ utils/utils.py
@@ -80,18 +80,18 @@ def latency_profiler(model, sample_arc, gpu=True, tensor=(1, 3, 224, 224)):
     del tensor
     return sum(times) / 100.0
 
-def cuda_latency_profiler(model, sample_arc, tensor=(1, 3, 224, 224)):
-    start = torch.cuda.Event(enable_timing=True)
-    end = torch.cuda.Event(enable_timing=True)
-    tensor = torch.randn(tensor).cuda(device=1)
+def cuda_latency_profiler(model, sample_arc, tensor=(1, 3, 224, 224), runs=10):
+    tensor = torch.randn(tensor, requires_grad=False).cuda(device=1)
     eval_model = copy.deepcopy(model)
     eval_model = eval_model.cuda(device=1)
-    eval_model = eval_model.eval()
-    start.record()
-    eval_model(tensor, sample_arc)
-    end.record()
-
-    # Waits for everything to finish running
-    torch.cuda.synchronize(device=1)
-
-    return start.elapsed_time(end)
+    eval_model.eval()
+    times = []
+    for _ in range(runs):
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+        eval_model(tensor, sample_arc)
+        end.record()
+        torch.cuda.synchronize(device=1)
+        times.append(start.elapsed_time(end))
+    return sum(times) / float(runs)
